b63d947290ce6441432411ed5b91d446fa813b01
zjsnh <3078035267@qq.com>
2024-07-09 17:18:56 +0800

    compile

---
+++ compile/mycode.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define M 100
+
+
+
+int main()
+{
+    printf("nihao\n");
+    printf("nihao: %d\n",M);
+    //printf("nihao: %d\n",M);
+    //printf("nihao: %d\n",M);
+    //printf("nihao: %d\n",M);
+    //printf("nihao: %d\n",M);
+
+    return 0;
+}
